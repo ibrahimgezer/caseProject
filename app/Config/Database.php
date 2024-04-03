0b437f9710db8ded9d2f00f4b285b3df0c067d12
ibrahimgezer <55022785+ibrahimgezer@users.noreply.github.com>
2024-04-03 21:25:33 +0300

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'teamwars',
+        'database'     => 'case',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
